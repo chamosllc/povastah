dfdfaacccfd77cbc7d456d0b61ad0e58ebbdc810
東田　雅宏 <mashiro@chamos.biz>
2021-07-04 10:24:14 +0900

    add comments

--- src/main/java/biz/chamos/povastah/Scene.java
+++ src/main/java/biz/chamos/povastah/Scene.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
-import java.util.Map;
 
 import com.change_vision.jude.api.inf.exception.InvalidUsingException;
 import com.change_vision.jude.api.inf.model.IDiagram;
@@ -21,84 +20,130 @@ import com.change_vision.jude.api.inf.presentation.ILinkPresentation;
 import com.change_vision.jude.api.inf.presentation.INodePresentation;
 import com.change_vision.jude.api.inf.presentation.IPresentation;
 
+/**
+ * POVRay Scene Launguage Script
+ *  astahダイアグラムの3DCG表現を出力する(POVRayシーン言語スクリプトファイルを出力する)
+ * @author mashiro@chamos.biz
+ * @since 2021/07/01
+ *
+ * @param <T> ダイアグラムの型
+ */
 public class Scene<T extends IDiagram> {
-	static protected String CR = System.lineSeparator();
+	static protected String CR = System.lineSeparator(); // 改行
 	static protected String HEADER_COMMENT = "/**" + CR
 			+ " * astah* Diagram 3D Visualization\n * %s %s" + CR
 			+ " * created at %s" + CR
 			+ " * presented by povastah" + CR + " **/" + CR + CR;
-	static final String COORDINATE = "<%.2f, %.2f, %.2f>";
+	static final String COORDINATE = "<%.2f, %.2f, %.2f>"; // 座標系フォーマット
 	
-	protected String projectName;
-	protected OutputStreamWriter sceneFile;
-	protected T diagram;
-	protected List<INodePresentation> nodes;
-	protected List<ILinkPresentation> links;
-	protected double depth;
-	
+	protected String projectName; // 未使用
+	protected OutputStreamWriter sceneFile; // シーン言語スクリプト出力ファイル
+	protected T diagram; // 対象ダイアグラム
+	protected List<INodePresentation> nodes; // ダイアグラム中のノード要素(Frame, Noteを除く)
+	protected List<ILinkPresentation> links; // ダイアグラム中のリンク要素
+	protected Rectangle2D stage; // nodesの含まれる矩形
+	protected double depth; // 3D表現時の奥行
+
 	@SuppressWarnings("unchecked")
 	public Scene(String projectName, INamedElement diagram, File directory) throws IOException {
 		this.projectName =projectName;
 		this.sceneFile = new OutputStreamWriter(new FileOutputStream(directory + File.separator + diagram.getName() + ".pov"), "UTF-8");
-		this.diagram = (T)diagram;	
+		this.diagram = (T)diagram;
+		this.stage = new Rectangle2D.Double();
 	}
-	
+
+	/**
+	 * astahダイアグラムの3DCG表現を出力する
+	 */
 	public void save(){
 		try {
 			extractElement();
 			writeHeader();
-			writeDeclare();
 			writeDiagram();
+			writeStage();
 			sceneFile.close();
 		} catch (IOException | InvalidUsingException e) {}
 	}
 
+	/**
+	 * ダイアグラムを出力する
+	 * @throws IOException
+	 */
 	protected void writeDiagram() throws IOException {
 		sceneFile.write("union {" + CR);
 		writeNodes();
 		writeLinks();
 		sceneFile.write("}" + CR);
 	}
+	
+	/**
+	 * スクリプトのヘッダ部を定義する
+	 * @throws IOException
+	 */
 	protected void writeHeader() throws IOException {
 		Calendar cl = Calendar.getInstance();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
 		sceneFile.write(String.format(HEADER_COMMENT, projectName, diagram.getName(), sdf.format(cl.getTime())));
 		sceneFile.write("#version 3.7" + CR + "#global_settings { assumed_gamma 2.2 }" + CR
 				+ "#global_settings { charset utf8 }" + CR + CR);	
-		sceneFile.write("#include \"uml.inc\"" + CR + CR);
+		sceneFile.write("#include \"astahuml.inc\"" + CR + CR);
 		sceneFile.flush();
 	}
-	
-	protected void writeDeclare() throws IOException {
+
+	/**
+	 * カメラ、光源、ダイアグラムのステージを定義する
+	 * @throws IOException
+	 */
+	protected void writeStage() throws IOException {
 		final String DEFVAR = "#declare %s = " + COORDINATE + ";" + CR;
-		Rectangle2D rect = diagram.getBoundRect();
-		depth = -(rect.getCenterX() + rect.getCenterY()) * 2;
-		sceneFile.write(String.format(DEFVAR, "EYE", rect.getCenterX(), rect.getCenterY(), depth/2));
-		sceneFile.write(String.format(DEFVAR, "FOCUS", rect.getCenterX(), rect.getCenterY(), 0.0));
+		depth = -(stage.getCenterX() + stage.getCenterY()) * 2;
+		sceneFile.write(String.format(DEFVAR, "EYE", stage.getCenterX(), -stage.getCenterY(), depth/2));
+		sceneFile.write(String.format(DEFVAR, "FOCUS", stage.getCenterX(), -stage.getCenterY(), 0.0));
 		sceneFile.write("camera { location EYE direction 1*z look_at FOCUS }" + CR
-				+ "light_source { <0, 128, -512> color White }" + CR
+				+ "light_source { EYE color White }" + CR
 				+ "plane { z, 32.0 texture { pigment { hexagon color Pink color White color SkyBlue } rotate -x*90 scale 64 } }" + CR);
 		sceneFile.flush();
 	}
-	
-	protected void writeNodes() throws IOException {
+
+	/**
+	 * ノード要素をマッピングする
+	 * @throws IOException
+	 */
+	protected void writeNodes() throws IOException {		
 		for (INodePresentation node : nodes) {
 			if(node.getType() != "Frame" && node.getType() != "Note" ) {
 				Point2D point = node.getLocation();
 				sceneFile.write("object { " + povrayObject(node) + " rotate -x*90 scale 16 " + translate(point) + " ");
-				writeProperties(node);
 				sceneFile.write("}" + CR);
-				if(!node.getType().contains("Initial") && !node.getType().contains("Final")) {
-					String label = node.getLabel();
-					point.setLocation(point.getX() - label.length()*5, point.getY() + 28 );
-					sceneFile.write(" text { ttf \"msgothic.ttc\", \"" + label + "\", 1, 0 scale 12 texture { T_Stone8 }"
-							+ CR + translate(point, -32.0) + " }" + CR);
-				}
+				writeLabel(node);
+				stage.add(node.getRectangle());
 			}
 		}
 		sceneFile.flush();
 	}
 
+	/**
+	 * ノードのラベルを加工する
+	 * @param node
+	 * @throws IOException
+	 */
+	protected void writeLabel(INodePresentation node) throws IOException {
+		Point2D point = node.getLocation();
+		if(!node.getType().contains("Initial") && !node.getType().contains("Final") && !node.getType().contains("Choice")) { // 名前が表示されない。デフォルトでついた名前を空にできない。
+			String label = node.getLabel();
+			point.setLocation(point.getX() - label.length()*5, point.getY() + 24 );
+			sceneFile.write(" text { ttf \"msgothic.ttc\", \"" + label + "\", 1, 0 scale 12 texture { T_Stone8 }"
+					+ CR + translate(point, -32.0) + " }" + CR);
+		}
+	}
+
+	/**
+	 * ノード要素の特定の型に対するPOVRayオブジェクトの型をマッピングする
+	 * 特定のステレオタイプについてはそのステレオタイプに対するPOVRayオブジェクトの型をマッピングする
+	 * 
+	 * @param node
+	 * @return
+	 */
 	protected String povrayObject(INodePresentation node) {
 		IElement model = node.getModel();
 		if(model != null) {
@@ -110,7 +155,7 @@ public class Scene<T extends IDiagram> {
 					return "Boundary";
 				}else if(types.contains("control")) {
 					return "Control";
-				} if(types.contains("entity")) {
+				}else if(types.contains("entity")) {
 					return "Entity";
 				}
 			}			
@@ -118,34 +163,45 @@ public class Scene<T extends IDiagram> {
 		return node.getType();
 	}
 
+	/**
+	 * リンク要素をマッピングする
+	 * astahダイアグラムのリンクのノードへの端点は使わない
+	 * 
+	 * @throws IOException
+	 */
 	protected void writeLinks() throws IOException {
+		double z = 4;
 		for (ILinkPresentation link : links) {
 			INodePresentation source = link.getSource();
 			INodePresentation target = link.getTarget();
 			if(source.getType() != "Note" && target.getType() != "Note") {
 				Point2D sourcep = source.getLocation();
 				Point2D targetp = target.getLocation();
-				if(sourcep.equals(targetp)) {
-					sourcep.setLocation(sourcep.getX(), sourcep.getY() - 48.0);
-					sceneFile.write("torus { 48, 3.0 rotate x*90 " + translate(sourcep));
+				sceneFile.write("// " + link.getType() + ":" + link.getLabel() + CR);
+				if(sourcep.equals(targetp)) { // 始点と終点が同じであればリレーションは真円にする
+					sourcep.setLocation(sourcep.getX(), sourcep.getY() - 36.0);
+					sceneFile.write("torus { 36, 3.0 rotate x*90 " + translate(sourcep, z));
 				}else {
-					sceneFile.write("union {" + CR);
 					Point2D[] points = link.getPoints();
-					sceneFile.write("  cylinder { " + coordinate(sourcep));			
-					for(int i=0; i < points.length - 1; i++) {
-						sceneFile.write(", " + coordinate(points[i]) + ", 3.0 }" + CR
-								+ "  cylinder { " + coordinate(points[i]));
+					sceneFile.write("sphere_sweep { linear_spline, " + points.length + ", " + CR); // 始点、終点の2点とpointsから最初と最後の2点を抜いた数の合計
+					sceneFile.write(coordinate(sourcep, z) + ", 3.0"+ CR); // 始点
+					for(int i=1; i < points.length - 1; i++) { // pointsの最初と最後の値を使わない(ノードの端点)
+						sceneFile.write(coordinate(points[i], z) + ", 3.0" + CR);
 					}
-					sceneFile.write(", " + coordinate(targetp) + ", 3.0 }" + CR);
+					sceneFile.write(coordinate(targetp, z) + ", 3.0" + CR); // 終点
 				}
-//				writeProperties(link);
 				sceneFile.write("  texture { T_Yellow_Glass }" + CR);
 				sceneFile.write("}" + CR);				
 			}
 		}
 
 	}
-	
+
+	/**
+	 * ダイアグラムのノード要素とリンク要素を抽出する
+	 * 
+	 * @throws InvalidUsingException
+	 */
 	protected void extractElement() throws InvalidUsingException {
 		nodes = new ArrayList<INodePresentation>();
 		links = new ArrayList<ILinkPresentation>();
@@ -158,47 +214,40 @@ public class Scene<T extends IDiagram> {
 		}
 	}
 
-	protected void writeProperties(INodePresentation presence) throws IOException {
-		Map<String, String> map = presence.getProperties();
-		for (String key : map.keySet()) {
-			if(key.endsWith(".color") || key.endsWith(".type")){
-				String value = map.get(key);
-				if (value != null && !value.isEmpty() && value != "null" && value != "normal") {
-					if(key.equals("fill.color")) {
-//						Color color = new Color(Integer.decode(value));
-//						sceneFile.write(String.format(" pigment { color rgb " + COORDINATE + "}", color.getRed()/256.0, color.getGreen()/256.0, color.getBlue()/256.0));
-					}
-//					sceneFile.write(key + "=" + value + " ");
-				}
-			}
-		}
-	}
-
-	protected void writeProperties(ILinkPresentation presence) throws IOException {
-		Map<String, String> map = presence.getProperties();
-		for (String key : map.keySet()) {
-			if(key.endsWith(".color") || key.endsWith(".type")){
-				String value = map.get(key);
-				if (value != null && !value.isEmpty() && value != "null" && value != "normal") {
-//					Color color = new Color(Integer.decode(value));
-//					sceneFile.write(String.format(" pigment { color rgb " + COORDINATE + "}", color.getRed()/256.0, color.getGreen()/256.0, color.getBlue()/256.0));
-				}
-			}
-		}
-	}
-	
+	/**
+	 * POVRayスクリプトのtranslate句を返す
+	 * @param point
+	 * @return
+	 */
 	protected String translate(Point2D point) {
 		return translate(point, 0.0);
 	}
 
+	/**
+	 * POVRayスクリプトのtranslate句を返す
+	 * @param point
+	 * @param z
+	 * @return
+	 */
 	protected String translate(Point2D point, double z) {
 		return " translate " + coordinate(point, z);
 	}
-	
+
+	/**
+	 * POVRayスクリプトのベクトル表記を返す
+	 * @param point
+	 * @return
+	 */
 	protected String coordinate(Point2D point) {
 		return coordinate(point, 0.0);
 	}
 	
+	/**
+	 * POVRayスクリプトのベクトル表記を返す
+	 * @param point
+	 * @param z
+	 * @return
+	 */
 	protected String coordinate(Point2D point, double z) {
 		return String.format(COORDINATE, point.getX(), -depth/2 - point.getY(), z);
 	}
--- src/main/java/biz/chamos/povastah/TranslateAction.java
+++ src/main/java/biz/chamos/povastah/TranslateAction.java
@@ -3,7 +3,6 @@ package biz.chamos.povastah;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import javax.swing.JFileChooser;
@@ -14,16 +13,21 @@ import com.change_vision.jude.api.inf.exception.ProjectNotFoundException;
 import com.change_vision.jude.api.inf.model.IActivityDiagram;
 import com.change_vision.jude.api.inf.model.IClassDiagram;
 import com.change_vision.jude.api.inf.model.IDiagram;
-import com.change_vision.jude.api.inf.model.IElement;
 import com.change_vision.jude.api.inf.model.IModel;
 import com.change_vision.jude.api.inf.model.INamedElement;
-import com.change_vision.jude.api.inf.model.IPackage;
 import com.change_vision.jude.api.inf.model.IStateMachineDiagram;
+import com.change_vision.jude.api.inf.model.IUseCaseDiagram;
 import com.change_vision.jude.api.inf.project.ModelFinder;
 import com.change_vision.jude.api.inf.project.ProjectAccessor;
 import com.change_vision.jude.api.inf.ui.IPluginActionDelegate;
 import com.change_vision.jude.api.inf.ui.IWindow;
 
+/**
+ * POVRayシーン言語スクリプトファイルを出力するアクション
+ * @author mashiro@chamos.biz
+ * @since 2021/07/01
+ *
+ */
 public class TranslateAction implements IPluginActionDelegate {
 	final static protected String FILE_EXT = "pov";
 	protected AstahAPI api;
@@ -39,9 +43,12 @@ public class TranslateAction implements IPluginActionDelegate {
 
 	        File directory = selectScriptOutputDirectory(window, project);
 	        if(directory != null) {
-		        pickDiagram(projectAccessor, directory);
+		        povrayDiagram(projectAccessor, directory);
 	        }
 	    } catch (ProjectNotFoundException e) {
+	    	/*
+	    	 *  プロジェクトがないときメニューを選択できないようにするにはどうすればよいか
+	    	 */
 	        String message = "プロジェクトがありません";
 			JOptionPane.showMessageDialog(window.getParent(), message, "Warning", JOptionPane.WARNING_MESSAGE); 
 	    } catch (Exception e) {
@@ -51,22 +58,49 @@ public class TranslateAction implements IPluginActionDelegate {
 	    return null;
 	}
 
-	protected void pickDiagram(ProjectAccessor accessor, File directory) throws IOException, ProjectNotFoundException {
+	/**
+	 * プロジェクト内のダイアグラムをPOVRayシーン言語スクリプトへ変換する
+	 * 
+	 * @param accessor
+	 * @param directory
+	 * @throws IOException
+	 * @throws ProjectNotFoundException
+	 */
+	protected void povrayDiagram(ProjectAccessor accessor, File directory) throws IOException, ProjectNotFoundException {
 		String projectName = projectAccessor.getProject().getName();
+		/*
+		 * プロジェクト中のすべてのユースケース図を出力する
+		 */
 //		for(INamedElement diagram: accessor.findElements(new IUseCaseDiagramPicker())){
 //			(new Scene<IUseCaseDiagram>(projectName, (IUseCaseDiagram)diagram, directory)).save();
 //		}
+		/**
+		 * プロジェクト中のすべてのクラス図を出力する
+		 */
 		for(INamedElement diagram: accessor.findElements(new IClassDiagramPicker())){
 			(new Scene<IClassDiagram>(projectName, (IClassDiagram)diagram, directory)).save();
 		}
+		/*
+		 * プロジェクト中のすべてのステートマシン図を出力する
+		 */
 		for(INamedElement diagram: accessor.findElements(new IStateMachineDiagramPicker())){
 			(new Scene<IStateMachineDiagram>(projectName, (IStateMachineDiagram)diagram, directory)).save();
 		}
+		/*
+		 * プロジェクト中のすべてのアクティビティ図を出力する
+		 */
 		for(INamedElement diagram: accessor.findElements(new IActivityDiagramPicker())){
 			(new Scene<IActivityDiagram>(projectName, (IActivityDiagram)diagram, directory)).save();
 		}
 	}
 
+	/**
+	 * 
+	 * @param window
+	 * @param project
+	 * @return
+	 * @throws ProjectNotFoundException
+	 */
 	protected File selectScriptOutputDirectory(IWindow window, IModel project) throws ProjectNotFoundException {
 		JFileChooser fileChooser = new JFileChooser((new File(projectAccessor.getProjectPath()).getParent()));
 		fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
@@ -81,38 +115,37 @@ public class TranslateAction implements IPluginActionDelegate {
 		}
 		return null;
 	}
-	
-	protected void searchDiagram(IPackage root){
-		diagrams.addAll(Arrays.asList(root.getDiagrams()));
-        for(IElement element: root.getOwnedElements()) {
-        	if(element instanceof IPackage) {
-        		searchDiagram((IPackage)element);
-        	}else {
-        		if(element instanceof INamedElement) {
-            		diagrams.addAll(Arrays.asList(((INamedElement)element).getDiagrams()));
-        		}
-        	}
-        }
+
+	/**
+	 * ユースケース図検索条件
+	 */
+	protected class IUseCaseDiagramPicker implements ModelFinder {
+	    public boolean isTarget(INamedElement namedElement) {
+	        return namedElement instanceof IUseCaseDiagram;
+	    }
 	}
-	
-//	protected class IUseCaseDiagramPicker implements ModelFinder {
-//	    public boolean isTarget(INamedElement namedElement) {
-//	        return namedElement instanceof IUseCaseDiagram;
-//	    }
-//	}
-	
+
+	/**
+	 * クラス図検索条件
+	 */
 	protected class IClassDiagramPicker implements ModelFinder {
 	    public boolean isTarget(INamedElement namedElement) {
 	        return namedElement instanceof IClassDiagram;
 	    }
 	}
 
+	/**
+	 * ステートマシン図検索条件
+	 */
 	protected class IStateMachineDiagramPicker implements ModelFinder {
 	    public boolean isTarget(INamedElement namedElement) {
 	        return namedElement instanceof IStateMachineDiagram;
 	    }
 	}
 
+	/**
+	 * アクティビティ図検索条件
+	 */
 	protected class IActivityDiagramPicker implements ModelFinder {
 	    public boolean isTarget(INamedElement namedElement) {
 	        return namedElement instanceof IActivityDiagram;
